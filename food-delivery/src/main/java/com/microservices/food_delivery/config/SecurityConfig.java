a2a2a3ba7e7c124a0b5848cc7b871f6c306ebbb4
akash-110405 <akashakash110405@gmail.com>
2026-01-31 12:01:57 +0530

    updated

--- food-delivery/src/main/java/com/microservices/food_delivery/config/SecurityConfig.java
+++ food-delivery/src/main/java/com/microservices/food_delivery/config/SecurityConfig.java
@@ -1,5 +1,6 @@
-package com.microservices.food_delivery.security;
+package com.microservices.food_delivery.config;
 
+import com.microservices.food_delivery.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
