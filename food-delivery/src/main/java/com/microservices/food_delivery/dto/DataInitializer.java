edbfaa1b80233bd916182d311adc3834c4f5f4b5
akash-110405 <akashakash110405@gmail.com>
2026-02-08 11:01:36 +0530

    updated

--- food-delivery/src/main/java/com/microservices/food_delivery/dto/DataInitializer.java
+++ food-delivery/src/main/java/com/microservices/food_delivery/dto/DataInitializer.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class DataInitializer  implements CommandLineRunner {
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
